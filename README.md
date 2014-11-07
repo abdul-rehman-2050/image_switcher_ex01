6a5b43868d8c454cfb0e3adc16f348961fffb8ed
Abdul Rehman <supermind2002@gmail.com>
2014-11-07 13:43:26 +0500

    source code added

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ image_switcher_ex01
 Image switcher to switch images in picturebox by clicking different button. Made in C# 2008
 
 
-Detail tutorial can be find fom https://www.projectolearning.wordpress.com/2014/11/06/image-switcher-in-c-sharp/
+Detail tutorial can be find fom http://www.projectolearning.wordpress.com/2014/11/06/image-switcher-in-c-sharp/
---
+++ picture_changer_ex01/picture_changer_ex01/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace picture_changer_ex01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image image = Image.FromFile("1.jpg");
+            pictureBox1.Image = image;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Image image = Image.FromFile("2.jpg");
+            pictureBox1.Image = image;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Image image = Image.FromFile("3.jpg");
+            pictureBox1.Image = image;
+        }
+    }
+}
